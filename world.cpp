b17f92419d1463cc7bf1fdc29958baa2860a372b
AshaLv <289574827@qq.com>
2020-05-21 16:34:55 +0800

    1.2

--- world.cpp
+++ world.cpp
@@ -15,23 +15,154 @@ void pre_populate_to_arr(vector<float> & arr);
 void linear_sort(vector<float> & arr);
 void linear_sort_k_elements(vector<float> & arr);
 float get_kth_maximum_element(const vector<float> & arr, int k);
+void print_words(vector<string> words);
+vector<string> find_valid_english_words(char* word_puzzle_board, string* dictionary);
+bool look_up_word_in_dictionary(string word, string* dictionary);
+
+enum class Dirctions_enum {
+	up = 0,
+	down,
+	left,
+	right,
+	left_up_diagonal,
+	right_up_diagonal,
+	left_down_diagonal,
+	right_down_diagonal
+};
 
 int main() {
-	vector<float> arr;
-	pre_populate_to_arr(arr);
+	char word_puzzle_board[] = {'t','h','i','s','w','a','t','s','o','a','h','g','f','g','d','t'};
+	string dictionary[4] = {"this","two","fat","that"};
 	long start_time = duration_cast< milliseconds >(
 		system_clock::now().time_since_epoch()
 	).count();
-	// linear_sort(arr);
-	linear_sort_k_elements(arr);
-	cout << "The kth maximum element is: " << get_kth_maximum_element(arr,NUM/2) << "\n\n";
+
+	vector<string> words = find_valid_english_words(word_puzzle_board,dictionary);
+	print_words(words);
+	
 	long end_time = duration_cast< milliseconds >(
 		system_clock::now().time_since_epoch()
 	).count();
-	cout << "The count of elements in this array: " << NUM << "\n";
 	cout << "The algorithm costs: " << end_time - start_time << " millisecond(s)" << "\n";
-	return 0;
 }
+
+void print_words(vector<string> words) {
+	for(int i = 0; i < 4; i++) {
+		cout << words[i] << "\t";
+	}
+	cout << "\n";
+}
+vector<string> find_valid_english_words(char* word_puzzle_board, string* dictionary) {
+	vector<string> words;
+	int rows = 4;
+	int cols = 4;
+	int index;
+	int next_index;
+	int max = 15;
+	int min = 0;
+	string word;
+	const Dirctions_enum directions[8] = {Dirctions_enum::up,Dirctions_enum::down,Dirctions_enum::left,Dirctions_enum::right,Dirctions_enum::left_up_diagonal,Dirctions_enum::right_up_diagonal,Dirctions_enum::left_down_diagonal,Dirctions_enum::right_down_diagonal};
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < cols; j++) {
+			index = i*cols + j;
+			for(int k = 0; k < 8; k++) {
+				word = word_puzzle_board[index];
+				switch(directions[k]) {
+					case 0:
+						next_index = index - cols;
+						while(next_index >= min && next_index <= max) {
+							word += word_puzzle_board[next_index];
+							if (look_up_word_in_dictionary(word,dictionary)) {
+								words.push_back(word);
+							}
+							next_index -= cols;
+						}
+						break;
+					case 1:
+						next_index = index + cols;
+						while(next_index >= min && next_index <= max) {
+							word += word_puzzle_board[next_index];
+							if (look_up_word_in_dictionary(word,dictionary)) {
+								words.push_back(word);
+							}
+							next_index += cols;
+						}
+						break;
+					case 2:
+						next_index = index - 1;
+						while(next_index >= i*cols && next_index <= max) {
+							word += word_puzzle_board[next_index];
+							if (look_up_word_in_dictionary(word,dictionary)) {
+								words.push_back(word);
+							}
+							next_index -= 1;
+						}
+						break;
+					case 3:
+						next_index = index + 1;
+						while(next_index >= min && next_index <= i*cols + 3) {
+							word += word_puzzle_board[next_index];
+							if (look_up_word_in_dictionary(word,dictionary)) {
+								words.push_back(word);
+							}
+							next_index += 1;
+						}
+						break;
+					case 4:
+						next_index = index - (cols + 1);
+						while(next_index >= min && next_index <= max) {
+							word += word_puzzle_board[next_index];
+							if (look_up_word_in_dictionary(word,dictionary)) {
+								words.push_back(word);
+							}
+							next_index -= (cols + 1);
+						}
+						break;
+					case 5:
+						next_index = index - cols + 1;
+						while(next_index >= min && next_index <= max) {
+							word += word_puzzle_board[next_index];
+							if (look_up_word_in_dictionary(word,dictionary)) {
+								words.push_back(word);
+							}
+							next_index = next_index - cols + 1;
+						}
+						break;
+					case 6:
+						next_index = index + cols - 1;
+						while(next_index >= min && next_index <= max) {
+							word += word_puzzle_board[next_index];
+							if (look_up_word_in_dictionary(word,dictionary)) {
+								words.push_back(word);
+							}
+							next_index = next_index + cols - 1;
+						}
+						break;
+					case 7:
+						next_index = index + cols + 1;
+						while(next_index >= min && next_index <= max) {
+							word += word_puzzle_board[next_index];
+							if (look_up_word_in_dictionary(word,dictionary)) {
+								words.push_back(word);
+							}
+							next_index = next_index + cols + 1;
+						}
+						break;
+				}
+			}
+		}
+	}
+	return words;
+}
+
+bool look_up_word_in_dictionary(string word, string* dictionary) {
+	for(int i = 0; i < 4; i++) {
+		if (word == dictionary[i]) 
+			return true;
+	}
+	return false;
+}
+
 
 void pre_populate_to_arr(vector<float> & arr) {
 	srand( (unsigned)time( NULL ) );
