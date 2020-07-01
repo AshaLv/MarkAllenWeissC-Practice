6c4a869aadbb4aa6a9918f73d56220594e0c96c6
AshaLv <289574827@qq.com>
2020-07-01 18:27:20 +0800

    3.4

--- application/VectorApplication.cpp
+++ application/VectorApplication.cpp
@@ -17,6 +17,26 @@ class VectorApplication {
                 L.push_back(*(p++));
             }
         } 
+        static const Vector<Object> common(const Vector<Object> & v1, const Vector<Object> & v2) {
+            // v1 and v2 are sorted 
+            int v1_size = v1.size();
+            int v2_size = v2.size();
+            Vector<Object> v;
+            int i = 0;
+            int j = 0;
+            while (i < v1_size && j < v2_size) {
+                Object v1_object = v1[i];
+                Object v2_object = v2[j];
+                if (v1_object == v2_object) {
+                    v.push_back(v1[i]);
+                    ++i;
+                    ++j;
+                } 
+                else if (v1_object > v2_object) ++j;
+                else ++i;
+            }
+            return v;
+        }
 };
 
 #endif
--- world.cpp
+++ world.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
-#include "list/SingleLinkedList.cpp"
-#include "list/DoubleLinkedList.cpp"
-#include "application/IterationApplication.cpp"
-
+// #include "list/SingleLinkedList.cpp"
+// #include "list/DoubleLinkedList.cpp"
+// #include "application/IterationApplication.cpp"
+#include "list/Vector.cpp"
+#include "application/VectorApplication.cpp"
 int main() {
 	//prepare data
-	DoubleLinkedList<int> double_linked_list;
-	double_linked_list.push_back(111);
-	double_linked_list.push_back(2);
-	double_linked_list.push_back(3);
-	double_linked_list.push_back(4);
-	DoubleLinkedList<int>::iterator begin = double_linked_list.begin();
-	DoubleLinkedList<int>::iterator end = double_linked_list.end();
+	Vector<int> v1;
+	Vector<int> v2;
+	int p1[] = {1,2,3,4,5,6,-1};
+	int p2[] = {3,44,666,-1};
+	VectorApplication<int>::populate(v1,p1);
+	VectorApplication<int>::populate(v2,p2);
 	//solve problem
-	cout << *(IterationApplication<DoubleLinkedList<int>::iterator, int>::find(begin, end, 344)) << "\n";
-	
+	Vector<int> v = VectorApplication<int>::common(v1,v2);
+	for (int i = 0; i < v.size(); i++) cout << v[i] << "\n";	
 }
 
 
